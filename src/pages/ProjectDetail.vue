b2fdbf75c2167bd88e9b35b9d26c78b1cb8537dd
SilvioGiacomelli <91.giacomelli.silvio@gmail.com>
2024-06-05 19:48:28 +0200

    refactor: Lazy load images in ProjectDetail.vue and improve API call

--- src/pages/ProjectDetail.vue
+++ src/pages/ProjectDetail.vue
@@ -1,12 +1,21 @@
 <script>
 import { store } from '../data/store';
 import axios from 'axios';
+import Main from '../components/Main.vue';
+import Loader from '../components/partials/Loader.vue';
 
 export default {
+  components:{
+    Main,
+    Loader,
+  
+  },
 name: 'ProjectDetail',
 data(){
   return{
     store,
+    project:{},
+    loading:true
   };
 },
 methods:{
@@ -14,9 +23,12 @@ methods:{
     const slug = this.$route.params.slug;
     axios.get(store.apiUrl + 'projects_by_slug/' + slug)
     .then(result => {
+      this.loading = false;
       console.log(result.data);
+      this.project=result.data.project;
     })
     .catch(error=>{
+      this.loading = false;
       console.log(error.message);
     })
   }
@@ -29,8 +41,22 @@ mounted(){
 
 <template>
   <div>
-    <h2>Ciao
-    </h2>
+    <h2>Project Detail</h2>
+    <Loader v-if="loading" />
+    <div v-else>
+      <h3>{{project.title}}</h3>
+      <p class="text">{{project.description}}</p>
+      <h4>Technologies</h4>
+      <ul>
+        <li v-for="(technology, index) in project.technologies" :key="index" class="btn btn-warning">{{ technology.title }}</li>
+      </ul>
+      <h4>Types</h4>
+      <ul>
+        <li v-for="(type, index) in project.types" :key="index">{{ type.title }}</li>
+      </ul>
+      <img :src="project.image">
+
+  </div>
   </div>
 </template>
 
